6df84d2cd5db356ec8d43ccbf6f6fc04da100da4
aaqib <aaqibnadeem123@gmail.com>
2022-11-26 02:29:14 +0500

    cosmetic changes

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -7,20 +7,20 @@ const ConnectWallet = () => {
 
     return (
         <div className="flex flex-col items-center justifiy-center mt-32">
-            <h2 className="font-bold lg:text-5xl text-3xl">Welcome to Chess Games</h2>
-            <p className="text-[#717171] text-base font-normal mt-1.5 lg:px-0 px-6">
+            <h2 className="font-bold lg:text-5xl text-3xl w-full text-center">Welcome to <br className="sm:hidden inline" /> Chess Games</h2>
+            <p className="text-[#717171] text-base font-normal mt-1.5 text-center">
                 Sign in with your wallet to be able to authenticate and play games
             </p>
-            <button onClick={() => activateBrowserWallet()} type="button" className="lg:w-96 w-auto outline-none flex items-center justify-start mt-16 bg-[#438FFE] rounded-[30px] lg:px-10 px-5 py-4">
+            <button onClick={() => activateBrowserWallet()} type="button" className="lg:w-96 w-full outline-none flex  items-center justify-start mt-16 bg-[#438FFE] rounded-[30px] lg:px-10 px-5 py-4">
                 <img width="26" src={MetamaskImg.src} alt="metamask" />
-                <p className="text-white lg:text-lg text-base font-semibold flex-1 text-center">
+                <p className="text-white tracking-wide lg:text-lg text-base font-semibold flex-1 text-center">
                     Sign in with metamask
                 </p>
             </button>
 
-            <button type="button" className="lg:w-96 w-auto outline-none flex items-center justify-start mt-5 rounded-[30px] border border-[#848484] lg:px-10 px-5 py-4">
+            <button type="button" className="lg:w-96 w-full outline-none flex  items-center justify-start mt-5 rounded-[30px] border border-[#848484] lg:px-10 px-5 py-4">
                 <img width="26" src={ConnectWalletImg.src} alt="connectwallet" />
-                <p className="text-black lg:text-lg text-base font-semibold flex-1 text-center">
+                <p className="text-black tracking-wide lg:text-lg text-base font-semibold flex-1 text-center">
                     Sign in with Coinbase Wallet
                 </p>
             </button>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,19 @@
 import { useEthers } from '@usedapp/core'
-
+import Image from 'next/image'
+import ChessImg from '../assets/images/chess.svg'
 const Header = () => {
     const { activateBrowserWallet, account, deactivate } = useEthers()
 
     return (
         <div className="flex items-center justify-between">
-            <h2 className="font-bold text-lg uppercase">Chess Games</h2>
+            <h2 className="font-bold text-xl uppercase flex items-center">
+                <p>Chess</p>
+                <Image width={30} src={ChessImg} alt="" style={{marginBottom: '10px'}} className="mx-1" />
+                <p>Games</p>
+            </h2>
             {account && (
                 <div className="">
-                    <div onClick={() => deactivate()} className="text-sm font-semibold cursor-pointer">{account.substring(0,5)}...{account.substring(37,42)}</div>
+                    <div onClick={() => deactivate()} className="text-sm font-semibold cursor-pointer">{account.substring(0, 5)}...{account.substring(37, 42)}</div>
                     <div></div>
                 </div>
             )}
